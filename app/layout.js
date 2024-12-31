f0da8a4dc534728f8067f59245f6db33f1138be0
Hyun Jaeyeon <jayhyun1008@gmail.com>
2024-12-31 12:56:59 +0900

    add about page, update main page and update css

--- app/layout.js
+++ app/layout.js
@@ -4,7 +4,13 @@ import "./aboutbar.css";
 
 export const metadata = {
   title: "제이에스 골프 에이전시",
-  description: "제이에스 골프 에이전시",
+  description: "백제 제이에스 컴퍼니/백제 골프 트레이닝 센터/골프 아카데미",
+  openGraph: {
+    url: "http://www.jsga.kr/",
+    title: "제이에스 골프 에이전시",
+    description: "백제 제이에스 컴퍼니/백제 골프 트레이닝 센터/골프 아카데미",
+    images: ["https://cdn.imweb.me/upload/S202004039b79029ae4742/e4b86a6b2d348.png"]
+  }
 };
 
 export default function RootLayout({ children }) {
@@ -13,20 +19,59 @@ export default function RootLayout({ children }) {
       <body>
         <div id="navbar">
           <div id="logo">
-            <img src="https://cdn.imweb.me/upload/S202004039b79029ae4742/1583c8a401770.png" />
+            <a href="/"><img src="https://cdn.imweb.me/upload/S202004039b79029ae4742/1583c8a401770.png" /></a>
           </div>
         </div>
         <div id="aboutbar">
-          <div id="about-left">
-            <div className="about-item"><a href="/about">About YS</a></div>
-            <div className="about-item"><a href="/works">Business</a></div>
-          </div>
-          <div id="about-right">
-            <div className="about-item"><a href="/goods">Community</a></div>
-            <div className="about-item"><a href="/subscribe">Contact</a></div>
+          <div>
+            <div id="about-left" className="bargroup">
+              <div className="about-item">
+                <a href="/about">About YS</a>
+                <div className="dropdown">
+                  <a href="/">About JS</a>
+                  <a href="/">Vision & Mission</a>
+                  <a href="/">History</a>
+                </div>
+              </div>
+              <div className="about-item">
+                <a href="/works">Business</a>
+                <div className="dropdown">
+                  <a href="/">Golf</a>
+                  <a href="/">Community Center</a>
+                  <a href="/">Screen Golf</a>
+                  <a href="/">Franchise</a>
+                  <a href="/">Management</a>
+                </div>
+              </div>
+            </div>
+            <div id="about-right" className="bargroup">
+              <div className="about-item">
+                <a href="/goods">Community</a>
+                <div className="dropdown">
+                <a href="/">News</a>
+                <a href="/">Event</a>
+                <a href="/">Gallery</a>
+                <a href="/">Communication</a>
+                </div>
+              </div>
+              <div className="about-item">
+                <a href="/subscribe">Contact</a>
+              </div>
+            </div>
           </div>
         </div>
         {children}
+        <footer>
+          <div>
+            <p><b>(주)육성그룹</b> / 대표 육종승</p>
+            <p>E-mail: jsga2020@gmail.com</p>
+            <p>Tel: 1660-3532 | Fax: 042-544-3533</p>
+            <p>Adr: 세종특별자치시 도움1로 7, 204호</p>
+            <p> </p>
+            <p>©2020, JS Golf Agency All Rights Reserved.</p>
+            <p>개인정보 보호 책임자: 404kill@naver.com</p>
+          </div>
+        </footer>
       </body>
     </html>
   );
--- app/page.js
+++ app/page.js
@@ -8,6 +8,91 @@ export default function Home() {
       <div id="bannermargin"></div>
       <fullwidth className={styles.fullwidth}>
         <main className={styles.main}>
+          <div className="littlewidth">
+            <img src="https://cdn.imweb.me/thumbnail/20230717/b03eb3912c8be.jpg"></img>
+          </div>
+          <div className={styles.flexgrow}>
+            <h1>KLPGA 신 비 선수</h1>
+            <div>
+              <p>JS Golf Agency는 꾸준한 후원 활동으로 선수의 능력을 파악하고 길러내며, 선수가 더욱이 자신의 실력을 발휘할 수 있도록 올바른 이정표가 되어 주고 있습니다. 그 중 우수한 수상 경력을 가진 KLPGA 소속 신 비 선수를 자세히 만나 보세요.</p>
+              <p><b>수상 경력</b></p>
+              <ul>
+                <li>2020 파워풀엑스 점프튜어 9차전 우승</li>
+                <li>2018 한국중고골프연맹 제29회 회장배 준우승</li>
+                <li>2014 제11회 한국초등학교골프연맹 회장배 전국 학생 골프대회 우승</li>
+              </ul>
+            </div>
+              <ctas className={styles.ctas}>
+                <a href="/works" className={styles.primary}>더보기</a>
+              </ctas>
+          </div>
+        </main>
+      </fullwidth>
+      <fullwidth className={styles.fullwidth}>
+        <main2 className={styles.main2}>
+            <h1>베스트 스크린 골프장</h1>
+            <gallery className={styles.gallery}>
+              <div>
+                <img src="https://cdn.imweb.me/thumbnail/20240321/9c94b59e605f7.jpeg" />
+              </div>
+              <div>
+                <img src="https://cdn.imweb.me/thumbnail/20240321/12e67ce81c204.jpeg" />
+              </div>
+              <div>
+                <img src="https://cdn.imweb.me/thumbnail/20240321/bd1537a7bfd71.jpeg" />
+              </div>
+              <div>
+                <img src="https://cdn.imweb.me/thumbnail/20240321/95bef63fa6e78.jpeg" />
+              </div>
+              <div>
+                <img src="https://cdn.imweb.me/thumbnail/20240321/7dae8d8fcd59b.jpeg" />
+              </div>
+              <div>
+                <img src="https://cdn.imweb.me/thumbnail/20240321/9d83f43fdd1c5.jpeg" />
+              </div>
+              <div>
+                <img src="https://cdn.imweb.me/thumbnail/20240321/bcea66fd30b36.jpeg" />
+              </div>
+              <div>
+                <img src="https://cdn.imweb.me/thumbnail/20240530/49d9f417d0533.jpeg" />
+              </div>
+            </gallery>
+            <ctas className={styles.ctas}>
+              <a href="/works" className={styles.primary}>더보기</a>
+            </ctas>
+        </main2>
+      </fullwidth>
+      <div className={styles.heading}>
+        <p>찾아가는 24시간 골프구조대</p>
+        <h1>GOLF119</h1>
+      </div>
+      <fullwidth className={styles.fullwidth}>
+        <main2 className={styles.main2}>
+            <iframe className="youtube" src="https://www.youtube.com/embed/9C3773tK2Ys?si=Gl_Klf-QGqoih5yA" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
+            <gallery className={styles.gallery2}>
+              <div>
+                <img src="https://cdn.imweb.me/thumbnail/20240424/bc0a898fb3905.jpg" />
+              </div>
+              <div>
+                <img src="https://cdn.imweb.me/thumbnail/20240424/5564ecdad8bd7.jpg" />
+              </div>
+              <div>
+                <img src="https://cdn.imweb.me/thumbnail/20240424/5da612c9d26d3.jpg" />
+              </div>
+              <div>
+                <img src="https://cdn.imweb.me/thumbnail/20240424/899c257095a19.jpg" />
+              </div>
+              <div>
+                <img src="https://cdn.imweb.me/thumbnail/20240424/6fa9063ce2b38.jpg" />
+              </div>
+            </gallery>
+            <ctas className={styles.ctas}>
+              <a href="/works" className={styles.primary}>더보기</a>
+            </ctas>
+        </main2>
+      </fullwidth>
+      <fullwidth className={styles.fullwidth}>
+        <main className={styles.main}>
           <div className={styles.flexgrow}>
             <h1>JS Golf Academy</h1>
             <div>
@@ -29,52 +114,41 @@ export default function Home() {
       </fullwidth>
       <fullwidth className={styles.fullwidth}>
         <main className={styles.main}>
-          <div className="littlewidth">
-            <img src="/books.jpeg"></img>
+          <div className={styles.midwidth}> 
+            <iframe className="youtube" src="https://www.youtube.com/embed/mSZGlyVqDo8?si=Gl_Klf-QGqoih5yA" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
           </div>
           <div className={styles.flexgrow}>
-            <h1>작품 읽기</h1>
+            <h1>백제 CC 골프 트레이닝 센터</h1>
             <div>
-              <p>온라인 업로드되는 작품 일부를 무료로 공개하고 있습니다.</p>
+              <p>충남 부여군 은산면 충절로 3734-82</p>
             </div>
               <ctas className={styles.ctas}>
                 <a href="/works" className={styles.primary}>더보기</a>
               </ctas>
           </div>
         </main>
-      </fullwidth>
-      <fullwidth className={styles.fullwidth}>
-        <main className={styles.main}>
-          <div className={styles.flexgrow}>
-            <h1>굿즈 구매</h1>
-            <div>
-              <p>각 텔러리움의 세계관이 담긴 굿즈들을 만나볼 수 있습니다.</p>
-            </div>
-              <ctas className={styles.ctas}>
-                <a href="/goods" className={styles.primary}>더보기</a>
-              </ctas>
-          </div>
-          <div className="littlewidth">
-            <img src="/goods.webp"></img>
-          </div>
-        </main>
-      </fullwidth>
-      <fullwidth className={styles.fullwidth}>
-        <main className={styles.main}>
-          <div className="littlewidth">
-            <img src="/zine.jpeg"></img>
-          </div>
-          <div className={styles.flexgrow}>
-            <h1>소책자 구독</h1>
-            <div>
-              <p>매월 세 명분의 텔러리움이 녹아 있는 작고 얇은 소책자를 배송해드려요.</p>
-              <p>아주 가끔씩은, 일러스트 굿즈나 수공예 아이템을 받을 수도 있답니다.</p>
-            </div>
-              <ctas className={styles.ctas}>
-                <a href="/subscribe" className={styles.primary}>더보기</a>
-              </ctas>
-          </div>
-        </main>
+        <main2 className={styles.main2}>
+            <gallery className={styles.gallery3}>
+              <div>
+                <img src="https://cdn.imweb.me/thumbnail/20211208/c39b6f2f24946.jpg" />
+              </div>
+              <div>
+                <img src="https://cdn.imweb.me/thumbnail/20211208/0418b23edd7d4.jpg" />
+              </div>
+              <div>
+                <img src="https://cdn.imweb.me/thumbnail/20211208/ea3c43c189c9e.jpg" />
+              </div>
+              <div>
+                <img src="https://cdn.imweb.me/thumbnail/20211208/0be95fec60274.jpg" />
+              </div>
+              <div>
+                <img src="https://cdn.imweb.me/thumbnail/20211208/056f78bc7d7d7.jpg" />
+              </div>
+              <div>
+                <img src="https://cdn.imweb.me/thumbnail/20211208/71faae8df1843.jpg" />
+              </div>
+            </gallery>
+        </main2>
       </fullwidth>
     </div>
   );
